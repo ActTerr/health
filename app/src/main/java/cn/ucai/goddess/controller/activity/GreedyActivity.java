94e9ca1268a29e45d5d0d89979ea73a248ae3777
mac-yk <164322105@qq.com>
2016-11-26 16:43:47 +0800

    改成随机的励志页面

--- app/src/main/java/cn/ucai/goddess/controller/activity/GreedyActivity.java
+++ app/src/main/java/cn/ucai/goddess/controller/activity/GreedyActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Random;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.goddess.R;
@@ -15,10 +17,12 @@ import cn.ucai.goddess.model.utils.MFGT;
 
 public class GreedyActivity extends BaseActivity {
     Context mContext;
+    int[] layouts=new int[]{R.layout.activity_greedy,R.layout.activity_greedy2,R.layout.activity_greedy3,R.layout.activity_greedy4};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_greedy);
+        Random random=new Random();
+        setContentView(layouts[random.nextInt(4)]);
         ButterKnife.bind(this);
         mContext=this;
     }
--- app/src/main/java/cn/ucai/goddess/controller/activity/HungryActivity.java
+++ app/src/main/java/cn/ucai/goddess/controller/activity/HungryActivity.java
@@ -3,7 +3,11 @@ package cn.ucai.goddess.controller.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
+import java.util.Random;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.goddess.R;
@@ -15,20 +19,31 @@ import cn.ucai.goddess.model.utils.MFGT;
 
 public class HungryActivity extends BaseActivity {
     Context mContext;
+    @BindView(R.id.ll_hungry)
+    LinearLayout llHungry;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hungry);
         ButterKnife.bind(this);
-        mContext=this;
+        mContext = this;
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        int drawables[]=new int[]{R.drawable.hungry,R.drawable.hungry2,R.drawable.hungry3,
+                R.drawable.hungry4,R.drawable.hungry5,R.drawable.hungry6,R.drawable.hungry7};
+        llHungry.setBackground(getResources().getDrawable(drawables[new Random().nextInt(7)]));
     }
 
     @OnClick(R.id.btn_continue)
     public void onClick() {
-        Intent intent=new Intent();
-        intent.putExtra("back",true);
-        intent.setClass(mContext,MainActivity.class);
-        MFGT.startActivity(mContext,intent);
+        Intent intent = new Intent();
+        intent.putExtra("back", true);
+        intent.setClass(mContext, MainActivity.class);
+        MFGT.startActivity(mContext, intent);
         finish();
     }
 }
